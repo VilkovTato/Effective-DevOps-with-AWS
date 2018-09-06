b42f928cf67f5ee9b692f8ce906bc0e648186384
root <root@ip-172-31-20-19.ec2.internal>
2018-09-06 17:38:45 +0000

    Chapters Added with Code Files

--- Chapter07/EffectiveDevOpsTemplates/helloworld-ecs-service-cf-template.py
+++ Chapter07/EffectiveDevOpsTemplates/helloworld-ecs-service-cf-template.py
@@ -2,9 +2,7 @@
 
 from troposphere.ecs import (
     TaskDefinition,
-    ContainerDefinition,
-    LogConfiguration,
-    Environment,
+    ContainerDefinition
 )
 from troposphere import ecs
 from awacs.aws import (
@@ -55,17 +53,7 @@ t.add_resource(TaskDefinition(
             Cpu=256,
             Name="helloworld",
             PortMappings=[ecs.PortMapping(
-                ContainerPort=3000)],
-            Environment=[
-                Environment(Name='HELLOWORLD_VERSION', Value=Ref("Tag"))
-            ],
-            LogConfiguration=LogConfiguration(
-                LogDriver="awslogs",
-                Options={
-                    'awslogs-group': "/aws/ecs/helloworld",
-                    'awslogs-region': Ref("AWS::Region"),
-                }
-            )
+                ContainerPort=3000)]
         )
     ],
 ))
